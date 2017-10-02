2edcdc147e96f119e2000aee29731cd8f37d53ab
Piotr Sekula <piotr92345@o2.pl>
2017-10-02 15:43:03 +0200

    Resolve sum of digits task

---
+++ src/main/java/SumDigits.java
@@ -0,0 +1,10 @@
+class SumDigits {
+
+    int sumDigits(int n) {
+        if (n % 10 == 0 && n < 10) {
+            return 0;
+        } else {
+            return n % 10 + sumDigits(n / 10);
+        }
+    }
+}
---
+++ src/test/java/SumDigitsTest.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SumDigitsTest {
+
+    private SumDigits sumDigits;
+
+    @DataProvider
+    public Object[][] provider() {
+        return new Object[][]{
+                {126, 9},
+                {49, 13},
+                {12, 3},
+                {10, 1},
+        };
+    }
+
+    @BeforeMethod
+    public void before(){
+        sumDigits = new SumDigits();
+    }
+
+    @Test(dataProvider = "provider")
+    public void testSumDigits(int n, int expectedResult) throws Exception {
+        // When
+        int result = sumDigits.sumDigits(n);
+
+        // Then
+        assertEquals(result, expectedResult);
+    }
+
+}
