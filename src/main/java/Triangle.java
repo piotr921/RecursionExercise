2b1b2204d884ad6994299779c36b9069d4f6a3b3
Piotr Sekula <piotr92345@o2.pl>
2017-10-02 15:31:36 +0200

    Resolve triangle problem

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,10 @@
+class Triangle {
+
+    int triangle(int rows) {
+        if (rows == 0) {
+            return 0;
+        } else {
+            return rows + triangle(rows - 1);
+        }
+    }
+}
---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,34 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TriangleTest {
+
+    private Triangle triangle;
+
+    @DataProvider
+    public Object[][] provider() {
+        return new Object[][]{
+                {0, 0},
+                {1, 1},
+                {2, 3},
+        };
+    }
+
+    @BeforeMethod
+    public void before() {
+        triangle = new Triangle();
+    }
+
+    @Test(dataProvider = "provider")
+    public void testTriangle(int rows, int expectedResult) throws Exception {
+        // When
+        int result = triangle.triangle(rows);
+
+        // Then
+        assertEquals(result, expectedResult);
+    }
+
+}
