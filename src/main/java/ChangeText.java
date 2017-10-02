7c6d870bc3b1fb8344039fd1b9df13e47973386d
Piotr Sekula <piotr92345@o2.pl>
2017-10-02 17:40:42 +0200

    Resolve change pi exercise

---
+++ src/main/java/ChangeText.java
@@ -0,0 +1,22 @@
+class ChangeText {
+
+    String changeXY(String str) {
+        if (str.isEmpty()) {
+            return "";
+        } else if (str.charAt(0) == 'x') {
+            return "y" + changeXY(str.substring(1));
+        } else {
+            return str.charAt(0) + changeXY(str.substring(1));
+        }
+    }
+
+    String changePi(String str) {
+        if (str.length() < 2){
+            return str;
+        } else if (str.substring(0,2).equals("pi")) {
+            return "3.14" + changePi(str.substring(2));
+        } else {
+            return str.charAt(0) + changePi(str.substring(1));
+        }
+    }
+}
---
+++ src/test/java/ChangeTextTest.java
@@ -0,0 +1,52 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ChangeTextTest {
+
+    private ChangeText changeText;
+
+    @DataProvider
+    public Object[][] providerXY() {
+        return new Object[][]{
+                {"codex", "codey"},
+                {"xxhixx", "yyhiyy"},
+                {"xhixhix", "yhiyhiy"},
+        };
+    }
+
+    @DataProvider
+    public Object[][] providerPi() {
+        return new Object[][]{
+                {"xpix", "x3.14x"},
+                {"pipi", "3.143.14"},
+                {"pip", "3.14p"},
+        };
+    }
+
+    @BeforeMethod
+    public void before() {
+        changeText = new ChangeText();
+    }
+
+    @Test(dataProvider = "providerXY")
+    public void testChangeXY(String text, String expectedResult) throws Exception {
+        // When
+        String result = changeText.changeXY(text);
+
+        // Then
+        assertEquals(result, expectedResult);
+    }
+
+    @Test(dataProvider = "providerPi")
+    public void testChangePi(String text, String expectedResult) throws Exception {
+        // When
+        String result = changeText.changePi(text);
+
+        // Then
+        assertEquals(result, expectedResult);
+    }
+
+}
