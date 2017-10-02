416b7db5002ed64c243d8e474e87ddc25633119a
Piotr Sekula <piotr92345@o2.pl>
2017-10-02 16:07:20 +0200

    Done power of N problem

---
+++ src/main/java/PowerN.java
@@ -0,0 +1,10 @@
+class PowerN {
+
+    int powerN(int base, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return base * powerN(base, n - 1);
+        }
+    }
+}
